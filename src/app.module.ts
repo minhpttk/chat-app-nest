91dc3cec0351abe21dc072b0b945824cc99db29a
minhpttk <dinhminh060102@gmail.com>
2025-03-24 19:04:22 +0700

    update source

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BookModule } from './book/book.module';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [
-    BookModule,
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
-    }),
-    MongooseModule.forRoot(process.env.DB_URL)
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
